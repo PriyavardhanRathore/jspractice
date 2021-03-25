0a8b26ebea28ea0ebf41ecdac4c996d36d4e63fc
PriyavardhanRathore <priyavarhanr99@gmail.com>
2021-03-25 20:54:13 +0530

    update script 5

--- script5.js
+++ script5.js
@@ -1,9 +1,13 @@
 function factorial(x) 
 {
 
-  if (x === 0)
+  if(x===0)
  {
-    return 1;
+    return (1);
+ }
+ else if(x<0)
+ {
+     console.log("please enter positive number");
  }
  else{
   num = x * factorial(x-1);
@@ -14,8 +18,8 @@ function factorial(x)
 //callback factorial function in the print function to show output
 function print(a , myCallback){
     console.time('Execution Time');
-    var b=myCallback(a);
+    let b=myCallback(a);
     console.timeEnd('Execution Time');
     console.log("The factorial of "+a+" is: "+b);
 }
-print(0, factorial);
+print(10, factorial);
